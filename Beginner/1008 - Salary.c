642605185e2a0e501937494bb65c4ae772ad30b1
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-07-01 22:34:17 +0600

    Create  1008 - Salary.c

---
+++ Beginner/1008 - Salary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n,m;
+float h , s;
+scanf("%d%d", &n , &m);
+scanf("%f", &h);
+s = (m * h) ;
+printf("NUMBER = %d\n" , n);
+printf("SALARY = U$ %.2f\n", s);
+}
