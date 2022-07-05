64aeb8a8d8f8fd371a80b5c7198dd8750736b09e
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-07-05 19:22:08 +0600

    Create  2738 - Contest.sql

---
+++ SQL/2738 - Contest.sql
@@ -0,0 +1,6 @@
+select candidate.name,round((((score.math*2)+
+(score.specific*3)+(score.project_plan*5))/10),2) as avg
+from candidate
+join score
+on candidate.id=score.candidate_id
+order by avg desc
