66365de1f364ba64052b3b0911fca48538d4f042
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-07-11 23:55:17 +0600

    Create 1060 - Positive Numbers.c

---
+++ Beginner/1060 - Positive Numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i , n = 0;
+    float a ;
+    for (i = 0; i < 6; i++)
+    {
+        scanf("%f", &a);
+        if(a >= 0)
+        n++;
+    }
+    printf("%d valores positivos\n", n);
+}
