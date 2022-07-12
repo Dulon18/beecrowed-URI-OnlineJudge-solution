74b5ddada922b6c6e2860d64ee8c6cf34768472e
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-07-12 22:43:44 +0600

    Create  1065 - Even Between five Numbers.c

---
+++ Beginner/1065 - Even Between five Numbers.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main()
+{
+    int i,n=0,x;
+    for(i=0;i<5;i++){
+        scanf("%d",&x);
+        if(x%2==0)
+        n++;
+    }
+    printf("%d valores pares\n",n);
+}
