55e0aa41099fc321ebd5d3ddc85e3dc27d877589
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-07-18 23:43:14 +0600

    Create  1113 - Ascending and Descending.cpp

---
+++ Beginner/1113 - Ascending and Descending.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X,Y,temp;
+    while(1){
+    cin>>X>>Y;
+    if(X>Y)
+      cout<<"Decrescente"<<endl;
+     else if(X==Y)
+    break;
+
+    else
+        cout<<"Crescente"<<endl;}
+
+}
