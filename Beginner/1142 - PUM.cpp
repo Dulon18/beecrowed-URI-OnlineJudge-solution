e161356b310909c66cd6d0f2d4762207f4d91d0e
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-07-24 23:26:47 +0600

    Create  1142 - PUM.cpp

---
+++ Beginner/1142 - PUM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+   int N,a=1;
+    cin>>N;
+    while(N--)
+    {
+     cout<<a<<" "<<a+1<<" "<<a+2<<" PUM"<<endl;
+     a+=4;
+    }
+ 
+    return 0;
+}
