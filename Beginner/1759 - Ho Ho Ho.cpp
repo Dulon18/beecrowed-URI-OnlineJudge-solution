ff3f1c79c69dde188d14802beb8dea46b8dca09f
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-08-03 22:07:14 +0600

    Create  1759 - Ho Ho Ho.cpp

---
+++ Beginner/1759 - Ho Ho Ho.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+ int i, N;
+    cin>>N;
+    for(i = 1; i < N; i++)
+    {
+        cout<<"Ho ";
+    }
+    cout<<"Ho!"<<endl;
+    return 0;
+}
