c32b6f5b59fe0fa881e854b785e64fb854f4044e
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-08-07 23:26:37 +0600

    Create 2234 - Hot Dogs.cpp

---
+++ Beginner/2234 - Hot Dogs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  int M,P;
+  cin>>M>>P;
+  float D=(float)M/P;
+  cout<< fixed << setprecision(2) <<D<<endl;
+ 
+
+ return 0;
+}
