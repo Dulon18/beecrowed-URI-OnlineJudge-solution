be37f3779be32825872ad4f99614a1e5454c14bf
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-08-07 23:24:31 +0600

    Create  2172 - Event.cpp

---
+++ Beginner/2172 - Event.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int x,i,m;
+    while(1){
+       cin>>x>>m;
+       if(x == 0 && m==0)
+        break;
+      cout<<x*m<<endl;
+
+        }
+        return 0;
+}
