f4878d332153ed0250f4e69df45aa8794c323b94
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-08-07 23:23:24 +0600

    Create  2006 - Identifying Tea.cpp

---
+++ Beginner/2006 - Identifying Tea.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ using namespace std;
+int main() {
+    
+    int T,A,B,C,D,E,i,c=0;
+    cin>>i;
+    cin>>A>>B>>C>>D>>E;
+    if(A==i)c++;
+    if(B==i)c++;
+    if(C==i)c++; 
+    if(D==i)c++;
+    if(E==i)c++;
+    cout<<c<<endl;
+    
+    return 0;
+}
