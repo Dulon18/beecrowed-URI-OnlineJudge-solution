0c2d36bf48ed3911d895bdc0914af964651c2bad
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-08-15 23:24:04 +0600

    Create  2581 - I am Toorg!.cpp

---
+++ Beginner/2581 - I am Toorg!.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int num,i;
+    char s[100];
+    cin>>num;
+    for (i = 0; i <=num; i++)
+    {
+
+        gets(s);
+        if(i!=0)
+        {
+            cout<<"I am Toorg!"<<endl;
+        }
+    }
+    return 0;
+
+}
