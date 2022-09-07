e3305c829303388159b236b42ae41a3d1b63d2ec
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-09-07 23:54:10 +0600

    Create 3302 - Correct Answer.cpp

---
+++ Beginner/3302 - Correct Answer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ int N,i,a;
+ cin>>N;
+ for(i=1; i<=N ;i++)
+ {   cin>>a;
+     cout<<"resposta "<<i<<": "<<a<<endl;
+ }
+  return 0;
+}
