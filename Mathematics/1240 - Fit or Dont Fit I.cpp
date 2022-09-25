25350f46dbc482a9c7a4f0c04c85d4ab06288536
Momotajur Rahman Dulan <80118217+Dulon18@users.noreply.github.com>
2022-09-25 22:43:18 +0600

    Create  1240 - Fit or Dont Fit I.cpp

---
+++ Mathematics/1240 - Fit or Dont Fit I.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int fitOrNot(long long num)
+{
+    int digits = 0;
+    
+    while (num) {
+        num /= 10;
+        digits++;
+    }
+    
+    return digits;
+}
+int main()
+{
+     int n;
+    long long x, y,m, z ;
+    string res;
+    
+    cin>>n;
+    
+    for(int i = 0; i < n; i++){
+            
+            cin>>x>>y;
+            z = fitOrNot(y);
+            z = ceil(pow(10,z));
+            m = x%z;
+            res=(m == y) ? "encaixa" : "nao encaixa";
+            cout<<res<<endl;
+    }
+
+    return 0;
+}
